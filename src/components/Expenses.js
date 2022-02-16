6d02df0d905388e939833fdb9fd69449f051a29a
Somshubro Chatterjee <somshubro20@gmail.com>
2022-02-17 01:24:26 +0530

    Added components and css

---
+++ src/components/Expenses.js
@@ -0,0 +1,28 @@
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css'
+
+const Expenses = (props) => {
+
+    return (
+        <div className="expenses">
+          <h2>Let's get started!</h2>
+          <ExpenseItem
+            title={props.expenses[0].title}
+            amount={props.expenses[0].amount}
+            date={props.expenses[0].date}
+          />
+          <ExpenseItem
+            title={props.expenses[1].title}
+            amount={props.expenses[1].amount}
+            date={props.expenses[1].date}
+          />
+          <ExpenseItem
+            title={props.expenses[2].title}
+            amount={props.expenses[2].amount}
+            date={props.expenses[2].date}
+          />
+        </div>
+      );
+}
+
+export default Expenses;
